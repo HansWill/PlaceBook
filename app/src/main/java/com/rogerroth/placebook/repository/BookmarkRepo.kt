d6eeb05de8ff98d17438a6e9084396c66a505a0c
RORoth <RORoth@PC-HEM00008219>
2019-12-09 12:50:26 +0100

    added categories to bookmarkrepo

--- app/src/main/java/com/rogerroth/placebook/repository/BookmarkRepo.kt
+++ app/src/main/java/com/rogerroth/placebook/repository/BookmarkRepo.kt
@@ -2,6 +2,8 @@ package com.rogerroth.placebook.repository
 
 import android.content.Context
 import androidx.lifecycle.LiveData
+import com.google.android.libraries.places.api.model.Place
+import com.rogerroth.placebook.R
 import com.rogerroth.placebook.db.BookmarkDao
 import com.rogerroth.placebook.db.PlaceBookDatabase
 import com.rogerroth.placebook.model.Bookmark
@@ -10,6 +12,8 @@ class BookmarkRepo(private val context: Context) {
 
 	private var db: PlaceBookDatabase = PlaceBookDatabase.getInstance(context)
 	private var bookmarkDao: BookmarkDao = db.bookmarkDao()
+	private var categoryMap: HashMap<Int, String> = buildCategoryMap()
+	private var  allCategories: HashMap<String, Int> = buildCategories()
 
 	fun updateBookmark(bookmark: Bookmark) {
 		bookmarkDao.updateBookmark(bookmark)
@@ -34,8 +38,55 @@ class BookmarkRepo(private val context: Context) {
 		return bookmark
 	}
 
+	fun placeTypeToCategory(placeType: Int): String {
+		var category = "Other"
+		if (categoryMap.containsKey(placeType)) {
+			category = categoryMap[placeType].toString()
+		}
+		return category
+	}
+
 	val allBookmarks: LiveData<List<Bookmark>>
 		get() {
 			return bookmarkDao.loadAll()
 		}
+
+	fun getCategoryResourceId(placeCategory: String): Int? {
+		return allCategories[placeCategory]
+	}
+
+	private fun buildCategories() : HashMap<String, Int> {
+		return hashMapOf(
+			"Gas" to R.drawable.ic_gas,
+			"Lodging" to R.drawable.ic_lodging,
+			"Other" to R.drawable.ic_other,
+			"Restaurant" to R.drawable.ic_restaurant,
+			"Shopping" to R.drawable.ic_shopping
+		)
+	}
+
+	private fun buildCategoryMap() : HashMap<Int, String> {
+		return hashMapOf(
+			Place.TYPE_BAKERY to "Restaurant",
+			Place.TYPE_BAR to "Restaurant",
+			Place.TYPE_CAFE to "Restaurant",
+			Place.TYPE_FOOD to "Restaurant",
+			Place.TYPE_RESTAURANT to "Restaurant",
+			Place.TYPE_MEAL_DELIVERY to "Restaurant",
+			Place.TYPE_MEAL_TAKEAWAY to "Restaurant",
+			Place.TYPE_GAS_STATION to "Gas",
+			Place.TYPE_CLOTHING_STORE to "Shopping",
+			Place.TYPE_DEPARTMENT_STORE to "Shopping",
+			Place.TYPE_FURNITURE_STORE to "Shopping",
+			Place.TYPE_GROCERY_OR_SUPERMARKET to "Shopping",
+			Place.TYPE_HARDWARE_STORE to "Shopping",
+			Place.TYPE_JEWELRY_STORE to "Shopping",
+			Place.TYPE_SHOE_STORE to "Shopping",
+			Place.TYPE_SHOPPING_MALL to "Shopping",
+			Place.TYPE_HOME_GOODS_STORE to "Shopping",
+			Place.TYPE_STORE to "Shopping",
+			Place.TYPE_LODGING to "Lodging",
+			Place.TYPE_ROOM to "Lodging"
+		)
+	}
 }
